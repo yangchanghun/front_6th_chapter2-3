eeb7e7abceb02989fe69100cb7d4e41c8ee92106
yangchanghun <gory4848@naver.com>
2025-08-14 23:00:08 +0900

    tanstack query App.tsx에

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/header/Header.tsx"
-import Footer from "./widgets/footer/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import Header from './widgets/header/Header.tsx';
+import Footer from './widgets/footer/Footer.tsx';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import PostsManagerPage from './pages/PostsManagerPage.tsx';
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className='flex flex-col min-h-screen'>
+          <Header />
+          <main className='flex-grow container mx-auto px-4 py-8'>
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
