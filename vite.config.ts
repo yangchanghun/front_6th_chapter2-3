69a85a0a79d99e5ec2bd13523a03ff8b4dc39bae
yangchanghun <gory4848@naver.com>
2025-08-15 01:48:31 +0900

    취소

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,16 @@
-// import { defineConfig } from "vite"
-// import react from "@vitejs/plugin-react"
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         // target: 'https://jsonplaceholder.typicode.com',
-//         target: "https://dummyjson.com",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-//     },
-//   },
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'https://jsonplaceholder.typicode.com',
+        target: 'https://dummyjson.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
